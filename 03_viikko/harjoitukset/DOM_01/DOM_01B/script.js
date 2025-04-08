d9d42c7e8ac987a0cd561e86ad7d8fd58e317aa1
Laura Järvinen <jarla@bc.fi>
2025-04-08 23:45:43 +0300

    Add DOM theory and first tasks

---
+++ 03_viikko/harjoitukset/DOM_01/DOM_01B/script.js
@@ -0,0 +1,7 @@
+/* Toteuta HTML sivu, jolla on nappi. Lisää tälle <button> elementille id. 
+Napin vieressä on numero nolla. Lisää tälle tekstipaikalle myös id. 
+
+Lisää napille getElementById:n avulla tapahtumakäsittelijä, joka kuuntelee 
+"click" -tapahtumaa. Kun nappia painetaan, verkkosivun numeron arvo kasvaa yhdellä. 
+
+*/
