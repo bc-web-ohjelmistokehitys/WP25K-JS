9697f7bc6e28efb4a574ec213b49e4110447b65a
Laura Järvinen <jarla@bc.fi>
2025-04-14 11:53:38 +0300

    add examples to basic dom week 3 exercises

---
+++ 03_viikko/harjoitukset/DOM_01/esimerkkivastaukset/DOM_01C/script.js
@@ -0,0 +1,14 @@
+/* Kopioi tähän koodi edellisestä tehtävästä. Kopioi myös tekemäsi HTML tiedosto tähän samaan kansioon. 
+
+Lisää sivulle toinen nappi, jota klikkaamalla laskurin arvo vähenee yhdellä. 
+
+Muista lisätä uudelle napille oma id ja sille oma tapahtumankuuntelija. Tee kaksi funktiota, jotka muuttavat saman muuttujan arvoa. 
+*/
+
+document.getElementById("lisaaNappi").addEventListener("click", function () {
+  document.getElementById("numero").textContent++;
+});
+
+document.getElementById("vahennaNappi").addEventListener("click", function () {
+  document.getElementById("numero").textContent--;
+});
